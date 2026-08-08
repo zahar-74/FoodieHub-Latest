378e694ef0d0324c69cd5e175c28f32c1b7604eb
Mazen Lasheen <mazen2203309@miuegypt.edu.eg>
2026-08-08 18:39:22 +0300

    Fix review route authentication middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
-const { protect, requireCustomer, requireAdmin } = require('../middleware/auth');
+const {
+    requireAuth,
+    requireRole
+} = require('../middleware/auth');
 
-router.post('/menu/:id/reviews', protect, requireCustomer, reviewController.createReview);
-router.get('/menu/:id/reviews', reviewController.getReviewsForItem);
-router.put('/reviews/:id', protect, reviewController.updateReview);
-router.delete('/reviews/:id', protect, reviewController.deleteReview);
-router.delete('/admin/reviews/:id', protect, requireAdmin, reviewController.adminDeleteReview);
+router.post(
+    '/menu/:id/reviews',
+    requireAuth,
+    requireRole('customer'),
+    reviewController.createReview
+);
+
+router.get(
+    '/menu/:id/reviews',
+    reviewController.getReviewsForItem
+);
+
+router.put(
+    '/reviews/:id',
+    requireAuth,
+    reviewController.updateReview
+);
+
+router.delete(
+    '/reviews/:id',
+    requireAuth,
+    reviewController.deleteReview
+);
+
+router.delete(
+    '/admin/reviews/:id',
+    requireAuth,
+    requireRole('admin'),
+    reviewController.adminDeleteReview
+);
 
 module.exports = router;
